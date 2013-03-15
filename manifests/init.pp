3f1b7bdad13b35f8fc864abed0f0e5efa144e61a
Tom <bballts14@gmx.de>
2013-03-15 15:14:59 +1100

    Fix exec definitions.

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,23 +10,23 @@ class pow {
   }
 
   # Set up firewall
-  exec {
+  exec { "set up firewall rules":
     command => "pow --install-system",
     user    => "root",
     unless  => "test -f /Library/LaunchDaemons/cx.pow.firewall.plist"
   }->
-  exec {
+  exec { "enable firewall launchd":
     command => "launchctl load -w /Library/LaunchDaemons/cx.pow.firewall.plist",
     user    => "root",
     unless  => "test -f /Library/LaunchDaemons/cx.pow.firewall.plist"
   }
 
   # launch at login
-  exec {
+  exec { "set up to start at login":
     command => "pow --install-local,
     unless  => "test -f ${home}/Library/LaunchAgents/cx.pow.powd.plist"
   }->
-  exec {
+  exec { "enable login launchd":
     command => "launchctl load -w ${home}/Library/LaunchAgents/cx.pow.powd.plist",
     unless  => "test -f ${home}/Library/LaunchAgents/cx.pow.powd.plist"
   }
