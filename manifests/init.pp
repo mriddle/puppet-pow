01fcd2bc103ecce23ecf7629dd00aa7f565b9e8d
Tom <bballts14@gmx.de>
2013-03-15 13:53:55 +1100

    Write pow config for rbenv.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,15 @@
 class pow {
   package { "pow": }
+
+  $home = "/Users/${::luser}"
+
+  file { "${home}/.powconfig":
+    ensure => present
+  }->
+  file_line { "make pow work with rbenv":
+    line    => "export PATH=$(rbenv root)/shims:$(rbenv root)/bin:$PATH",
+    path    => "${home}/.powconfig",
+    require => Package["pow"]
+  }
 }
 
