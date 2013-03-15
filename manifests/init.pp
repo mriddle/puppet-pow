9c211411deb4dfd178567fb079698a3dfb9a738b
Tom <bballts14@gmx.de>
2013-03-15 14:11:25 +1100

    Ensure we have a .pow directory.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,12 @@ class pow {
 
   $home = "/Users/${::luser}"
 
+  file { "${home}/.pow":
+    target  => "${home}/Library/Application Support/Pow/Hosts",
+    ensure  => "link",
+    require => Package["pow"]
+  }
+
   file { "${home}/.powconfig":
     ensure => present
   }->
